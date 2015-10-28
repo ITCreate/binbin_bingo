d5e305a3115d07f2ff8ccf3b53118f815c327298
Takahiro Morimoto <tera09@me.com>
2015-10-28 23:11:47 +0900

    modify 使いやすさup

--- regist.js
+++ regist.js
@@ -5,14 +5,14 @@ var fs = require('fs');
 function send(data){
 
 request.post({
-	url: 'http://127.0.0.1:1081/bingo',
+	url: 'http://104.155.200.193//bingo',
 	form: JSON.stringify(data)
 }, function(err, httpres, body){
 console.log(err, httpres, body);
 });
 
 }
-fs.readFile('./data/json.json', 'utf8' ,function(err, text){
+fs.readFile('/tmp/result6.json', 'utf8' ,function(err, text){
 	var obj = JSON.parse(text);
 	obj.forEach(function(obj){
 		send(obj);	
